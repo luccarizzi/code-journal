0a7f4ce512d992c2a49016aa4b6c5b687646380d
Lucca Rizzi <luccarizzi29@gmail.com>
2020-12-02 12:47:54 -0600

    Update the avatar preview image's src when the avatar url input changes. #1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
 var $avatarUrlInput = document.getElementById('avatarUrl');
+var $profileImage = document.getElementById('profileImage');
 
 $avatarUrlInput.addEventListener('input', function (e) {
-
+  var imgUrl = event.target.value;
+  $profileImage.setAttribute('src', imgUrl);
 });
