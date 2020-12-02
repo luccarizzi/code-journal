160ec24e59ea6e8a7eb10d697d70c15a8ac2bbac
Lucca Rizzi <luccarizzi29@gmail.com>
2020-12-02 13:35:41 -0600

    Listen for 'submit' events on the form. #1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 
-var $avatarUrlInput = document.getElementById('avatarUrl');
+var $avatarUrl = document.getElementById('avatarUrl');
 var $profileImage = document.getElementById('profileImage');
 
-$avatarUrlInput.addEventListener('input', function (e) {
+$avatarUrl.addEventListener('input', function (e) {
   var imgUrl = event.target.value;
   $profileImage.setAttribute('src', imgUrl);
+});
+
+var $submit = document.querySelector('.input-submit');
+
+$submit.addEventListener('click', function (e) {
+  e.preventDefault();
 });
