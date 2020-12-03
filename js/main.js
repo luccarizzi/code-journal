25027ebd9dae4af1bb5f2d6c472ebf518de3125c
Lucca Rizzi <luccarizzi29@gmail.com>
2020-12-02 20:46:03 -0600

    Define a JavaScript function that takes the profile data model and returns a DOM tree that matches the HTML. #2

--- js/main.js
+++ js/main.js
@@ -18,3 +18,58 @@ $form.addEventListener('submit', function (e) {
   $form.reset();
   $profileImage.setAttribute('src', 'images/placeholder-image-square.jpg');
 });
+
+function renderProfile(object) {
+
+  var section = document.createElement('section');
+
+  var divColumnFull = document.createElement('div');
+  divColumnFull.setAttribute('class', 'column-full');
+
+  var divRow = document.createElement('div');
+  divRow.setAttribute('class', 'row');
+
+  var h1 = document.createElement('h1');
+  h1.textContent = object.profile.fullName;
+
+  var divColumnHalf1 = document.createElement('div');
+  divColumnHalf1.setAttribute('class', 'column-half profile-image-container');
+
+  var divColumnHalf2 = document.createElement('div');
+  divColumnHalf2.setAttribute('class', 'column-half font-18');
+
+  var img = document.createElement('img');
+  img.setAttribute('class', 'profile-image');
+  img.setAttribute('src', object.profile.avatarUrl);
+  img.setAttribute('alt', 'Profile image.');
+
+  var div1 = document.createElement('div');
+  var div2 = document.createElement('div');
+
+  var p1 = document.createElement('p');
+  p1.textContent = object.profile.username;
+
+  var p2 = document.createElement('p');
+  p2.textContent = object.profile.location;
+
+  var p3 = document.createElement('p');
+  p3.textContent = object.profile.bio;
+
+  var i1 = document.createElement('i');
+  i1.setAttribute('class', 'fas fa-user');
+
+  var i2 = document.createElement('i');
+  i2.setAttribute('class', 'fas fa-map-marker-alt');
+
+  section.append(divColumnFull, divRow);
+  divColumnFull.append(h1);
+  divRow.append(divColumnHalf1, divColumnHalf2);
+  divColumnHalf1.append(img);
+  divColumnHalf2.append(div1, div2, p3);
+  div1.append(i1, p1);
+  div2.append(i2, p2);
+
+  return section;
+}
+
+renderProfile(data);
