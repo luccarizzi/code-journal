05ff978bec1955fa98cdb7dcb2082158a205ba4c
Lucca Rizzi <luccarizzi29@gmail.com>
2020-12-04 13:19:02 -0600

    Test the user flow in a private browsing tab. #3

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ var $profileImage = document.getElementById('profileImage');
 $avatarUrl.addEventListener('input', function (e) {
   var imgUrl = event.target.value;
   $profileImage.setAttribute('src', imgUrl);
+  if ($avatarUrl.value === '') {
+    $profileImage.setAttribute('src', 'images/placeholder-image-square.jpg');
+  }
 });
 
 var $form = document.getElementById('form');
@@ -16,8 +19,9 @@ $form.addEventListener('submit', function (e) {
     var formName = formElements[i].name;
     data.profile[formName] = formElements[i].value;
   }
-  $form.reset();
-  $profileImage.setAttribute('src', 'images/placeholder-image-square.jpg');
+
+  // $profileImage.setAttribute('src', 'images/placeholder-image-square.jpg');
+  // $form.reset();
   swapView('profile');
 });
 
